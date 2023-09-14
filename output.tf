68fc3716c5e521a9879f7d9982c470f7e747c3da
Amar <amarnath3058@gmail.com>
2023-09-14 16:09:54 +0530

    y

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
-output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = module.vpc.vpc_id
-}
-
-output "internet_gateway" {
-  description = "The ID of the Internet Gateway."
-  value       = module.vpc.internet_gateway_id
-}
-
 output "public_subnet_az1_id" {
   description = "The ID of the public subnet in Availability Zone 1."
   value       = module.vpc.public_subnet_az1_id
