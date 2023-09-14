5b9ce8a25c0072ba09270c6b3a80256f013b4033
Amar <amarnath3058@gmail.com>
2023-09-14 16:29:57 +0530

    ss

--- variables.tf
+++ variables.tf
@@ -20,16 +20,20 @@ variable "vpc_cidr" {
 
 variable "pub_subnet_az1_cidr" {
   description = "CIDR block for the public subnet in Availability Zone 1."
+  default     = "10.0.1.0/24"  # You can adjust the default value as needed.
 }
 
 variable "pub_subnet_az2_cidr" {
   description = "CIDR block for the public subnet in Availability Zone 2."
+  default     = "10.0.2.0/24"  # You can adjust the default value as needed.
 }
 
 variable "prvt_app_subnet_az1_cidr" {
   description = "CIDR block for the private application subnet in Availability Zone 1."
+  default     = "10.0.10.0/24"  # You can adjust the default value as needed.
 }
 
 variable "prvt_app_subnet_az2_cidr" {
   description = "CIDR block for the private application subnet in Availability Zone 2."
+  default     = "10.0.11.0/24"  # You can adjust the default value as needed.
 }
