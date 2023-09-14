b5d8b655dbe6d3db8635c0b366579a1795231739
Amar <amarnath3058@gmail.com>
2023-09-14 09:30:31 +0530

    vpc module

---
+++ vpc/variables.tf
@@ -0,0 +1,13 @@
+# Environment variable 
+variable "region" {}
+variable "project_name" {}
+variable "Environment" {}
+
+# VPC variables
+variable "vpc_cidr" {}
+variable "pub_subnet_az1_cidr" {}
+variable "pub_subnet_az2_cidr" {}
+variable "prvt_app_subnet_az1_cidr" {}
+variable "prvt_app_subnet_az2_cidr" {}
+variable "prvt_data_subnet_az1_cidr" {}
+variable "prvt_data_subnet_az2_cidr" {}
