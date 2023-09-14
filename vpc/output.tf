7b23cd3e53340fdf524b3232bed0a00ed9dda5bb
Amar <amarnath3058@gmail.com>
2023-09-14 15:51:02 +0530

    chng

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,9 +1,9 @@
-output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.vpc.id
-}
+#output "vpc_id" {
+ # description = "The ID of the VPC."
+ # value       = aws_vpc.vpc.id
+#}
 
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway."
-  value       = aws_internet_gateway.internet_gateway.id
-}
+#output "internet_gateway_id" {
+  #description = "The ID of the Internet Gateway."
+  #value       = aws_internet_gateway.internet_gateway.id
+#}
